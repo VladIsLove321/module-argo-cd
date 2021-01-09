bf3d04f43d88aa23d87b50a1cdee668bb0c3654c
VladIsLove321 <s7edgevlad@gmail.com>
2021-01-09 20:28:04 +0600

    ArgoCD module init

---
+++ main.tf
@@ -0,0 +1,36 @@
+
+provider "kubernetes" {
+  load_config_file       = false
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", var.kubernetes_cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", var.kubernetes_cluster_name]
+    }
+  }
+}
+
+resource "kubernetes_namespace" "argo-ns" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "msur"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = "argocd"
+}
